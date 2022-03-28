a9006e920ce7694ea062b0b0697311e30812f85a
CarlosRodrigoSchimanski <76493107+CarlosRodrigoSchimanski@users.noreply.github.com>
2022-03-28 13:49:42 -0300

    a

--- exercicio4.html/js/config.js
+++ exercicio4.html/js/config.js
@@ -1,14 +1,22 @@
-const ant = document.getElementById('anterior');
-const pos = document.getElementById('posterior');
+'use strict';
 
+const imagens = [
+    {'id': '1', 'url': './imagens/banner.png'},
+    {'id': '2', 'url': './imagens/banner2.png'},
+    {'id': '3', 'url': './imagens/banner3.png'},
+    {'id': '4', 'url': './imagens/banner4.png'}
+]
 
-function befor() {
-    slider.src='./imagens/banner3.png';
+const container = document.querySelector('.container')
+
+const loadimagens = (imagens, container) => {
+    imagens.forEach(image => {
+        container.innerHTML +=`
+            <div class='item'>
+                <img src='${image.url}'
+            </div>
+        `
+    });
 }
 
-function after() {
-    slider.src='./imagens/banner2.png';
-}
-
-pos.addEventListener('click', befor);
-ant.addEventListener('click', after);
+loadimagens( imagens, container);
