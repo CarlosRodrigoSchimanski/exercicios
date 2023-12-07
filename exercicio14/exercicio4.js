949eaa5bc2971f95e8ee2f3714ca96766f323e5f
CarlosRodrigoSchimanski <76493107+CarlosRodrigoSchimanski@users.noreply.github.com>
2023-12-07 18:33:47 -0300

    exercicio 4 refatorado

--- exercicio14/exercicio4.js
+++ exercicio14/exercicio4.js
@@ -4,8 +4,6 @@ Faça um programa que receba do usuário um array de números. Faça
 uma função que recebe o array e retorna outro array com os valores em
 ordem invertida. (sem usar o .reverse())
 */
-let numeros = []
-let controle = 0
 
 const inverteLista = (lista) => {
     let novalista = []
@@ -17,13 +15,19 @@ const inverteLista = (lista) => {
     return novalista
 }
 
-while (controle !== -1){
-    io.write('digite um numero')
-    numeros.push(io.readInt())
-    
-    io.write('se quiser parar digite -1')
-    controle = io.readInt()
+const lista = () => {
+    let controle = 0
+    let numeros = []
+
+    while (controle !== -1){
+        io.write('digite um numero')
+        numeros.push(io.readInt())
+        
+        io.write('se quiser parar digite -1')
+        controle = io.readInt()
+    }
+    return numeros
 }
 
-let invertida = inverteLista(numeros)
+let invertida = inverteLista(lista())
 io.write(invertida)
